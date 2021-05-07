07413e7ff5f37055c8aa9f3488ab6e5496dda5da
abdullahbodur <abdullahbodur.abbdr@gmail.com>
2021-05-07 22:21:15 +0300

    added logger

--- gtm/server.py
+++ gtm/server.py
@@ -1,22 +1,24 @@
 from .data.config import Config
 from binance.client import Client
-from .trader.binance_api_manager import Binance_API_Manager
+from .api.binance_api_manager import Binance_API_Manager
 from .data.database.database_manager import DatabaseManager
 from .scheduler import SafeScheduler
 from .trader.trader import Trader
+from .data.logger import Logger
 import time
 
 
 class Server:
-
     def start(self):
 
         manager = Binance_API_Manager()
 
         db_manager = DatabaseManager()
 
-        trader = Trader(manager,db_manager)
-        
+        logger = Logger("server")
+
+        trader = Trader(manager, db_manager, logger)
+
         print("Server Started\n")
 
         trader.startTrade()
